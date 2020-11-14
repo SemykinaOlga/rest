8b69bc793c251a4179b0b7894bb26453e4d49d0a
Olga Semykina <semikinaolga@mail.ru>
2020-11-14 11:00:53 +0300

    add matchesJsonSchema

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -19,7 +19,6 @@ class MobileBankApiTestV4 {
       .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
     }
 }
